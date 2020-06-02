e89066c6bae01b3d8d9765a914c6401f537c6737
Trohat <petr.sed@centrum.cz>
2020-06-02 12:08:10 +0200

    Left and down rotations added

--- rotations.js
+++ rotations.js
@@ -8,6 +8,8 @@ function addRotateListener(cube, id, animationName) {
 const cube = document.querySelector("#cube");
 
 addRotateListener(cube, "right", "rotate-right");
-addRotateListener(cube, "up", "rotate-top");
+addRotateListener(cube, "left", "rotate-left");
+addRotateListener(cube, "up", "rotate-up");
+addRotateListener(cube, "down", "rotate-down");
 addRotateListener(cube, "z-axis-rotation", "rotate-z");
 addRotateListener(cube, "stop", "none");
